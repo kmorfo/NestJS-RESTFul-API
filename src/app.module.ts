d1f70be206750a0bce563dc139b4bc1bb2557c41
Raúl Luján Colilla <mcdrokz@gmail.com>
2023-04-13 17:35:53 +0200

    Teslo rc:1.1

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,12 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl: process.env.STAGE === 'prod'
+          ? { rejectUnauthorized: false }
+          : null,
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
