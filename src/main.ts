35496acf9e59a59f512201c291244831eaac2f8f
Raúl Luján Colilla <mcdrokz@gmail.com>
2023-08-06 09:41:30 +0200

    main.ts Añadido CORS para permitir conexiones externas

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const logger = new Logger('Bootstrap');
 
   app.setGlobalPrefix('api');
